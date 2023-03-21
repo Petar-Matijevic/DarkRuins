3bedb2bd6fa8252e6b3fc0fe1d6eea2e487777ca
Petar Matijević <66-2019@ftn.edu.rs>
2023-03-21 19:58:14 +0100

    s

---
+++ code/magic.py
@@ -0,0 +1,12 @@
+import pygame
+from settings import *
+
+class MagicPlayer:
+    def __init__(self,animation_player):
+        self.animation_player = animation_player
+
+    def heal(selfs):
+        pass
+
+    def flame(selfs):
+        pass
