5e4ebf0f26a4a0c499683ef6586088055c28109e
Petar Matijević <66-2019@ftn.edu.rs>
2023-03-21 18:06:25 +0100

    Initial commit

---
+++ level/levels/tilesets/Tress.tsx
@@ -0,0 +1,64 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Tress" tilewidth="128" tileheight="128" tilecount="20" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="128" height="128" source="../../../../../Desktop/05 - level graphics/5 - level graphics/graphics/objects/0.png"/>
+ </tile>
+ <tile id="1">
+  <image width="128" height="128" source="../../../../../Desktop/05 - level graphics/5 - level graphics/graphics/objects/01.png"/>
+ </tile>
+ <tile id="2">
+  <image width="128" height="128" source="../../../../../Desktop/05 - level graphics/5 - level graphics/graphics/objects/02.png"/>
+ </tile>
+ <tile id="3">
+  <image width="128" height="128" source="../../../../../Desktop/05 - level graphics/5 - level graphics/graphics/objects/03.png"/>
+ </tile>
+ <tile id="4">
+  <image width="128" height="128" source="../../../../../Desktop/05 - level graphics/5 - level graphics/graphics/objects/04.png"/>
+ </tile>
+ <tile id="5">
+  <image width="128" height="128" source="../../../../../Desktop/05 - level graphics/5 - level graphics/graphics/objects/05.png"/>
+ </tile>
+ <tile id="6">
+  <image width="128" height="128" source="../../../../../Desktop/05 - level graphics/5 - level graphics/graphics/objects/06.png"/>
+ </tile>
+ <tile id="7">
+  <image width="128" height="128" source="../../../../../Desktop/05 - level graphics/5 - level graphics/graphics/objects/07.png"/>
+ </tile>
+ <tile id="8">
+  <image width="128" height="128" source="../../../../../Desktop/05 - level graphics/5 - level graphics/graphics/objects/08.png"/>
+ </tile>
+ <tile id="9">
+  <image width="128" height="128" source="../../../../../Desktop/05 - level graphics/5 - level graphics/graphics/objects/09.png"/>
+ </tile>
+ <tile id="10">
+  <image width="128" height="128" source="../../../../../Desktop/05 - level graphics/5 - level graphics/graphics/objects/10.png"/>
+ </tile>
+ <tile id="11">
+  <image width="64" height="128" source="../../../../../Desktop/05 - level graphics/5 - level graphics/graphics/objects/11.png"/>
+ </tile>
+ <tile id="12">
+  <image width="64" height="128" source="../../../../../Desktop/05 - level graphics/5 - level graphics/graphics/objects/12.png"/>
+ </tile>
+ <tile id="13">
+  <image width="64" height="128" source="../../../../../Desktop/05 - level graphics/5 - level graphics/graphics/objects/13.png"/>
+ </tile>
+ <tile id="14">
+  <image width="64" height="128" source="../../../../../Desktop/05 - level graphics/5 - level graphics/graphics/objects/14.png"/>
+ </tile>
+ <tile id="15">
+  <image width="128" height="128" source="../../../../../Desktop/05 - level graphics/5 - level graphics/graphics/objects/16.png"/>
+ </tile>
+ <tile id="16">
+  <image width="128" height="128" source="../../../../../Desktop/05 - level graphics/5 - level graphics/graphics/objects/17.png"/>
+ </tile>
+ <tile id="17">
+  <image width="128" height="128" source="../../../../../Desktop/05 - level graphics/5 - level graphics/graphics/objects/18.png"/>
+ </tile>
+ <tile id="18">
+  <image width="128" height="128" source="../../../../../Desktop/05 - level graphics/5 - level graphics/graphics/objects/19.png"/>
+ </tile>
+ <tile id="19">
+  <image width="128" height="128" source="../../../../../Desktop/05 - level graphics/5 - level graphics/graphics/objects/20.png"/>
+ </tile>
+</tileset>
